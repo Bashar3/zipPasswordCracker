139647358c4fa09e67ebaa18a78e67680a8d9b3a
git.nuclear <38399691+Bashar3@users.noreply.github.com>
2021-07-24 04:16:18 +0200

    Updated version of the tool.
    Fixed the issue of the crashing and added a nice ascii pic.

--- zipBruteForcer.py
+++ zipBruteForcer.py
@@ -4,8 +4,19 @@ import sys
 from time import time
 
 AUTHOR = "Bashar Oumari"
-VERSION = "1.0"
-
+VERSION = "2.0"
+art = print("""\
+                         v
+                   (__)v | v
+                   /\/\\_|_/
+                  _\__/  |
+                 /  \/`\<`)
+                 \ (  |\_/
+                 /)))-(  |
+                / /^ ^ \ |
+               /  )^/\^( |
+               )_//`__>> |
+                 #   #`  |  """)
 parser = argparse.ArgumentParser(description="\nUsage: python zipBruteForcer.py -z <zipfile.zip> -p <passwordfile.txt>"+
                                  "\n-V " + VERSION + " by " + AUTHOR )
 
@@ -13,6 +24,7 @@ parser.add_argument("-z", dest="ziparchive", help="Zip archive file")
 parser.add_argument("-p", dest="passfile", help="passwordFile")
 
 parsed_args = parser.parse_args()
+
 
 try:
     ziparchiver = ZipFile(parsed_args.ziparchive)
@@ -31,9 +43,11 @@ except Exception:
     print("Error: wordlist not found!")
     sys.exit(1)
 c_t = time()
-
+i = 0
 with open(passfile, "r") as f:
-    for line in f:
+    passes = f.readlines()
+    print("Testing every password in the list Please be patient...")
+    for line in passes:
         password = line.strip("\n")
         password = password.encode("utf-8")
 
@@ -42,10 +56,14 @@ with open(passfile, "r") as f:
             tT = time() - c_t
             if foundpass1 is None:
                 print("\n# Found password:", password.decode())
-                print("\n# Took", tT, "seconds to crack the password")
+                print("\n# Took", round(tT, 2), "seconds to crack the password")
                 print()
-        except RuntimeError:
+                quit()
+        except Exception:
             pass
 
     if foundpass1 == "":
         print("\nPassword not found. Try a bigger password list.")
+        quit()
+
+
